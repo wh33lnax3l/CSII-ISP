ca5e753299ea4c504ebcafc8a337913bac1059d5
Ethan Forbes <ethan.forbes@student.allenisd.org>
2021-02-25 11:00:03 -0600

    Added database and JSON file

--- Package.swift
+++ Package.swift
@@ -9,13 +9,14 @@ let package = Package(
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/TheCoderMerlin/Curses.git", from: "1.0.0"),
+    .package(name: "PerfectSQLite", url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", from: "3.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "CSII-ISP",
-      dependencies: ["Curses"],
+      dependencies: ["Curses", "PerfectSQLite"],
       path: "Sources"),    
   ]
 )
--- Sources/CSII-ISP/DBHelper.swift
+++ Sources/CSII-ISP/DBHelper.swift
@@ -1,8 +1,36 @@
+///////////////////////////////////////////////////////////////////
+// Code copied from https://github.com/TheCoderMerlin/SQLiteTest //
+///////////////////////////////////////////////////////////////////
+
+import Foundation
+import PerfectSQLite
+
+// Determine pathname of database
+let currentDirectory = URL(fileURLWithPath: FileManager.default.currentDirectoryPath, isDirectory: true)
+let targetPathname = currentDirectory.appendingPathComponent("pokemon.db", isDirectory: true)
+guard FileManager.default.fileExists(atPath: targetPathname.path) else {
+    fatalError("Unable to find database file: \(targetPathname.path)")
+}
+
+do {
+    let sqlite = try SQLite(targetPathname.path)
+    defer {
+        sqlite.close()
+    }
+
+    let statement = "SELECT * FROM albums"
+    try sqlite.forEachRow(statement: statement) {(statement: SQLiteStmt, i:Int) -> () in
+        print(statement.columnText(position: 1))
+    }
+} catch {
+    fatalError(error.localizedDescription)
+}
+
+/*
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Code copied from https://gist.github.com/imbilalhassan/8cb9de7eb242947934a996bd68f34bee#file-dbhelper-swift //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*
 import Foundation
 
 class DBHelper{
